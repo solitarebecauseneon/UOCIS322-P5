f9ff0ef318ea4eee85b117990ba3b9e3341cfbe3
James Taylor <solitarebecauseneon@gmail.com>
2021-05-24 20:45:17 -0700

    U44 V3: implemented basic display route in flask_brevets.py; rough outline of acptimes.html; rough implementation of display function in calc.html; error handling for submit function and page output

--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -6,7 +6,7 @@ Replacement for RUSA ACP brevet time calculator
 
 import os
 import flask
-from flask import request
+from flask import request, render_template
 import json
 import arrow  # Replacement for datetime, based on moment.js
 import acp_times  # Brevet time calculations
@@ -59,6 +59,12 @@ def insert():
         db.timestable.insert_one(control_point)
     result = "Success!"
     return flask.jsonify(result=result)
+
+
+@app.route('/_display_route')
+def index():
+    return render_template('acptimes.html',
+                           items=list(db.timestable.find()))
 
 
 ###############
